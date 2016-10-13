3d053c451f86786dd3954857d9c6473c8416a72e
ulfsigvardsson <ulf_sigvardsson@hotmail.com>
2016-10-13 15:32:03 +0200

    Add files via upload
    Lagt till spelare och optimerat sökalgoritmen.

--- maze.py
+++ maze.py
@@ -1,147 +1,312 @@
 from tile import *
+import pygame
+pygame.init()
 
-def has_neigbour_floor(list, x, y):
-    """Determines if a tile has neighbouring floors, to be used for maze generation"""
-    neighbourlist=neighbour_list(list, x, y)
-    for neighbour in neighbourlist:
-        if neighbour.type == "Floor":
-            return True
-        #TODO: connect to a maze generating function
 
-def neighbour_list(list, x, y):
-    """Returns a list of all neighbouring tiles in four directions"""
-    neighbours = [list[x][y + 1], list[x][y - 1], list[x + 1][y], list[x - 1][y]]
+
+# =============================================
+#                   FUNCTIONS
+# =============================================
+
+def neighbour_list(x, y):
+
+    """Returns a list of all neighbouring tiles in four directions
+
+        x:: integer, x-coordinate of a Tile-object
+        y:: integer, y-coorfinate of a Tile-object"""
+
+    neighbours = [
+                    row_list[x][y + 1],
+                    row_list[x][y - 1],
+                    row_list[x + 1][y],
+                    row_list[x - 1][y]
+    ]
+
     return neighbours
 
-def findpath(tile, x, y):
+
+def paint_path(tile):
+
+    """Prints the optimal path towards the goal by back-tracking from the goal tile
+
+    tile:: instance of Tile"""
+
+    if tile.type == "Player":      # If it is the player tile, reverse the list
+
+        path.reverse()
+
+
+        for tile in path:          # Then follow the path to the goal
+
+            time.sleep(0.01)
+
+            if tile.type == "Exit":
+                tile.setFill("green")
+
+            else:
+                tile.setFill("yellow")
+
+    else:
+        path.append(tile.parent)
+        paint_path(tile.parent)
+
+
+def solve(tile, x, y):
+
     """Moves the 'player' through the maze
 
        tile:: Tile-object representing the current position
        x:: integer, x-coordinates of 'tile'
        y:: integer, y-coordinates of 'tile'    """
 
+    closed_list.append(tile)
+
+    if tile in open_list:
+        open_list.remove(tile)                  # Make tile uneligible for future consideration
+
+
     if tile.type == "Exit":
-        tile.setFill("green")
-        print "Success!"
-        w1.getMouse()   # Keeps the window from closing
+        path.append(tile)
+        paint_path(tile)
+        w1.getMouse()                           # Keeps the window from closing
         quit()
 
     else:
-        time.sleep(0.03)    # Time delay to enable visual tracking
-        tile.setFill("yellow")
         tile.set_status("Visited")
-        neighbours = neighbour_list(row_list, x, y)     # Finds all neighbouring tiles and returns them in a list
+        neighbours = neighbour_list(x, y)       # Finds all neighbouring tiles
+                                                # and returns them in a list
 
-        neighbours.sort(key=lambda x: x.distance)   # Sorts the list of neighbours by distance to goal
 
-        for neighbour in neighbours:    # Runs the findpath() function on all neighbours that is not a wall or have already been visited
-            if neighbour.status != "Visited" and neighbour.type != "Wall":
-                findpath(neighbour, neighbour.get_x(), neighbour.get_y())
+        for neighbour in neighbours:
 
-def generate_map(tile, x, y):
-    """Generates a random map """
+            if neighbour.type != "Wall" and neighbour.status != "Visited":
+                open_list.append(neighbour)     # Adds all valid neighbours to open_list
+                neighbour.set_parent(tile)
+                neighbour.cost = tile.cost+1      # Increment the travel cost of the neighbours by one
 
-    # TODO: Change this copy of find_path to an actual mapgenerator
-    
-    time.sleep(0.03)    # Time delay to enable visual tracking
-    tile.setFill("white")
-    tile.set_type("Floor")
-    neighbours = neighbour_list(row_list, x, y)     # Finds all neighbouring tiles and returns them in a list
+                neighbour.total_cost = neighbour.cost*neighbour.distance    # Set total cost to travel cost
+                                                                            # times distance to goal
 
-    neighbours.sort(key=lambda x: x.distance)   # Sorts the list of neighbours by distance to goal
+        open_list.sort(key=lambda x: x.total_cost)    # Sorts the list of open tiles by distance to goal
 
-    for neighbour in neighbours:    # Runs the findpath() function on all neighbours that is not a wall or have already been visited
-        if neighbour.status != "Visited" and neighbour.type != "Wall":
-            findpath(neighbour, neighbour.get_x(), neighbour.get_y())
+        new_tile = open_list[0]
+        solve(new_tile, new_tile.get_x(), new_tile.get_y())  # Runs the solve() function on the open tile
+                                                                # closest to the goal
 
 
 def make_goal(tile):
-    """Establishes a goal tile
+
+    """Establishes a goal tile and returns its coordinates
 
        tile:: a Tile-object"""
 
-    x = tile.get_x()
-    y = tile.get_y()
-    coordinates=Point(x, y)
+    x = tile.get_x()            # x-coordinates for the tile
+    y = tile.get_y()            # y-coordinates for the tile
+    coordinates = Point(x, y)   # Coordinates for the goal tile as a Point()
     tile.setFill("red")
-    tile.type="Exit"
+    tile.type = "Exit"
     return coordinates
 
-#  ==============================================================================================
-#                                       MAIN METHOD
-#  ==============================================================================================
 
-map = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 3, 1, 0, 0, 1, 1, 0, 1],
-        [1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1],
-        [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1],
-        [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1],
-        [1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1],
-        [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1],
-        [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1],
-        [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1],
-        [1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1],
-        [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1],
-        [1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 2, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-] # A hard-coded map
+def make_maze():
 
-# CONSTANTS, DO NOT CHANGE THESE!!!
-ROWS = 20
-COLUMNS = 20
-WINDOWHEIGHT = 500
-WINDOWBREADTH = 500
+    """Generates a list of tile-objects with attributes according to the hard coded map"""
+
+    for i in range(len(map)):  # Outer loop for generating every row
+        column_list = []  # List set to empty for a iteration of a new row
+
+        for j in range(len(map[i])):  # Inner loop to iterate every column on each row
+
+            # Tile object are created as floors by default
+            r = Tile(
+                Point(i * TILESIZE, j * TILESIZE),  # Upper corner of Tile
+                Point((i + 1) * TILESIZE, (j + 1) * TILESIZE),  # Lower corner of Tile
+                "Floor",  # Tile type
+                i,  # x-coordinate in the grid
+                j  # y-coordinate in the grid
+            )
+
+            # Paints the tiles according to the map
+
+            if map[j][i] == 1:
+
+                r.setFill("black")
+                r.set_type("Wall")
+
+
+            elif map[j][i] == 3:
+
+
+                r.setFill("blue")
+                r.set_type("Player")
+                # Player tile, not yet implemented
+
+
+
+
+            elif map[j][i] == 2:
+
+                r.setFill("red")
+                r.set_type("Exit")
+                goal = r
+                goal_coords = make_goal(goal)  # Sets coordinates for the goal tile
+
+            column_list.append(r)  # Append each rectangle to its list of columns
+            r.draw(w1)
+
+        row_list.append(column_list)  # Append each list to the list of rows
+
+        # Sets the distance-to-goal-attribute for each tile on the map
+    for i in range(len(row_list)):
+
+        for j in range(len(column_list)):
+            row_list[i][j].set_distance(goal)
+
+
+def callback(event):
+
+    """Event handler for click events, sets focus to the display screen"""
+
+    w1.focus_set()
+
+
+def key(event):
+
+    """Event handler for pressed keys"""
+
+    current=get_current()                       # Get the current player tile
+    x=current.get_x()                           # Get the x-coordinate
+    y=current.get_y()                           # Get the y-coordinate
+
+    if x>0 and x<COLUMNS and y>0 and y<ROWS:    # Check if it's possible to make a move within the boundaries of the map
+        up = row_list[x][y-1]
+        down = row_list[x][y+1]
+        left = row_list[x-1][y]
+        right = row_list[x+1][y]
+
+        if event.keysym == "s":                   # If 's' is pressed, the bot solves the maze
+            solve(current, x, y)
+
+        if event.keysym == "Up":                  # If any of the arrow keys are pressed,
+                                                  # move accordingly
+            if up.type != "Wall":
+                up.setFill("blue")
+                up.type = "Player"
+                current.setFill("white")
+                current.type = "Floor"
+
+        if event.keysym == "Down":
+
+            if down.type != "Wall":
+                down.setFill("blue")
+                down.type = "Player"
+                current.setFill("white")
+                current.type = "Floor"
+
+        if event.keysym == "Left":
+
+            if left.type != "Wall":
+                left.setFill("blue")
+                left.type = "Player"
+                current.setFill("white")
+                current.type = "Floor"
+
+        if event.keysym == "Right":
+
+            if right.type != "Wall":
+                right.setFill("blue")
+                right.type = "Player"
+                current.setFill("white")
+                current.type = "Floor"
+
+
+def get_current():
+
+    """Returns the current player tile from row_list"""
+
+    for row in row_list:
+        for tile in row:
+            if tile.type == "Player":
+                return tile
+
+
+
+# =============================================
+#                 BACKING FIELD
+# =============================================
+
+map = [                             #
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 3, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+        [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+        [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+        [1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+        [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1],
+        [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1],
+        [1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+        [1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+        [1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1],
+        [1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1],
+        [1, 2, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]         # A hard-coded map
+
+ROWS = 40
+COLUMNS = 40
+WINDOWHEIGHT = 1000
+WINDOWBREADTH = 1000
 TILESIZE = WINDOWBREADTH/COLUMNS
 
-w1 = GraphWin("Maze", WINDOWHEIGHT, WINDOWBREADTH)
+w1 = GraphWin(
+                "Maze",
+                WINDOWHEIGHT,   # Height of window
+                WINDOWBREADTH   # Breadth of window
+)
+
 row_list = []       # List to contain every list of columns
-goal = None
-start = None
+goal = None         # Declared in main scope for availability
+start = None        # Declared in main scope for availability
+open_list = []      # Tracks all current path options
+closed_list = []    # Tracks all exhausted paths
+path = []           # Stores the optimal path
 
 
-for i in range(len(map)): # Outer loop for generating every row
-    column_list = []  # List set to empty for a iteration of a new row
 
-    for j in range(len(map[i])):   # Inner loop to iterate every column on each row
+# =============================================
+#                   MAIN
+# =============================================
 
-        # Tile object are created as floors by default
-        r = Tile(Point(i*TILESIZE, j*TILESIZE), Point((i+1)*TILESIZE, (j+1)*TILESIZE), "Floor", i, j)
+make_maze()         # Display the initial map
 
-        if map[j][i] == 1:  # Paints the tiles according to the map
-            r.setFill("black")
-            r.set_type("Wall")
+while True:
 
-        elif map[j][i] == 3:
-            r.setFill("blue")
-            r.set_type("Start")
-            start = r
-
-        elif map[j][i] == 2:
-            r.setFill("red")
-            r.set_type("Exit")
-            goal = r
-            goal_coords = make_goal(goal)   # Sets coordinates for the goal tile
-
-        column_list.append(r)   # Append each rectangle to its list of columns
-        r.draw(w1)
-    row_list.append(column_list)    # Append each list to the list of rows
-
-# Sets the distance-to-goal-attribute for each tile on the map
-for i in range(len(row_list)):
-    for j in range(len(column_list)):
-        row_list[i][j].set_distance(goal)
-
-#TODO: write a maze generating function ta determine the exit tile before storing them in an array.
-
-findpath(start, start.get_x(), start.get_y())
-w1.getMouse()
-
-
+    w1.bind("<Key>", key)               # Sends all key events to the event handler
+    w1.bind("<Button-1>", callback)     # Sends all mouse events to the event handler, sets focus to the window
+    w1.getMouse()                       # Prevents the window from closing
--- tile.py
+++ tile.py
@@ -2,9 +2,14 @@ from graphics import *
 import math
 
 class Tile(Rectangle):
+
     """Derives from Rectangle but adds among others a type-attribute to distinguish between walls, floors and goal"""
-    status = "undiscovered" # Initial status for all tiles
-    distance = 0    # Initial distance for all tiles
+
+    status = "undiscovered"     # Initial status for all tiles
+    distance = 0                # Initial distance for all tiles
+    parent = None
+    cost = 0                    # When a tile is discovered, cost is set equal to distance traveled from the start tile
+    total_cost = 0              # When a tile is discovered, total cost is set to self.cost*self.distance
 
     def __init__(self, p1, p2, type, x, y):     # Constructor
         Rectangle.__init__(self, p1, p2)
@@ -13,35 +18,60 @@ class Tile(Rectangle):
         self.y = y
 
     def get_type(self):
+
         """Returns the type-attribute of a Tile-object"""
+
         return self.type
 
+    def get_parent(self):
+
+        """Returns the parent-attribute of a Tile-object"""
+
+        return self.parent
+
+    def set_parent(self, parent):
+        self.parent=parent
+
     def get_status(self):
+
         """Returns the status-attribute of a Tile-object"""
+
         return self.status
 
     def set_type(self, type):
+
         """Sets the type-attribute of a Tile-object"""
+
         self.type=type
 
     def set_status(self, status):
+
         """Sets the status-attribute of a Tile-object"""
+
         self.status=status
 
     def get_x(self):
+
         """Returns the x-coordinate of a Tile-object"""
+
         return  self.x
 
     def get_y(self):
+
         """Returns the y-coordinate of a Tile-object"""
+
         return self.y
 
     def get_distance(self):
+
         """Returns the distance between a Tile-object and the goal tile"""
+
         return self.distance
 
     def set_distance(self, goal):
+
         """Sets the distance between a Tile-object and the goal tile"""
+
         self.distance = math.sqrt((self.get_x() - goal.get_x()) ** 2 + (self.get_y() - goal.get_y()) ** 2)
 
 
